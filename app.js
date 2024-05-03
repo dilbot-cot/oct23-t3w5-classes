d668c21a66e6b6e4750a71f414003df12a08e86e
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-05-04 09:39:00 +1000

    feat: class examples

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// Import Library class, Book class, Audiobooks class, Games class, Movies class, Music class
+const { Media } = require("./Media")
+// Create instances of classes
+// Lord of the Rings as an instance of Book class
+
+let lotr = new Media("fellowship of the ring")
+console.log(lotr)
+
+// Do things with those instances
+
+// Create an instance of a Library
+// add media instances into the Library
+
+// Log the data in the library
