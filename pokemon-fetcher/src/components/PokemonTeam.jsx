9b73a539f2108d29071b8c61700c4f12ff234b92
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-05-04 10:42:50 +1000

    feat: fetch data on component mount

---
+++ pokemon-fetcher/src/components/PokemonTeam.jsx
@@ -0,0 +1,55 @@
+
+
+import React from "react";
+
+export default class PokemonTeam extends React.Component {
+	constructor(props){
+		super(props);
+
+        this.state = {
+            pokemonTeam: []
+        }
+	}
+
+	// Make requests to PokeAPI
+	// on component load 
+	async componentDidMount() {
+
+		function getRandomPokemonId(){
+			return Math.floor(Math.random() * 1025) + 1;
+		}
+
+		let randomNumber = getRandomPokemonId();
+		console.log(randomNumber);
+
+		let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + randomNumber).catch(error => {
+			console.log(error);
+			return error;
+		});
+		let data = await response.json();
+		console.log(data.name);
+
+        // Invalid way to update
+        //this.state.pokemonTeam.push(data)
+        // Valid way to update stateL
+        this.setState({pokemonTeam: [...this.state.pokemonTeam, data]})
+	}
+
+	// Store data from each request
+
+	// Pass each data to PokemonCard components 
+
+	// How do we render HTML in class component?
+	render(){
+		return(
+			<div>
+				<h1>Pokemon data here</h1>
+
+                {this.state.pokemonTeam && this.state.pokemonTeam.map((pokemon, index) => {
+                    return <h1 key={index}>{pokemon.name}</h1>
+                })}
+			</div>
+		)
+	}
+
+}
---
+++ pokemon-fetcher/src/pages/App.js
@@ -0,0 +1,15 @@
+
+import PokemonTeam from '../components/PokemonTeam';
+import '../stylesheets/App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon Team Generator</h1>
+
+      <PokemonTeam />
+    </div>
+  );
+}
+
+export default App;
